a5560f63f56a327b5b9aa944ee5e6122da6abcf5
unknown <e0015290@ISSBOR21108.iss.nus.edu.sg>
2016-08-31 15:37:13 +0800

    CR02 fix regression issue with CR01.

--- src/sg/edu/nus/iss/vmcs/maintenance/MaintenanceController.java
+++ src/sg/edu/nus/iss/vmcs/maintenance/MaintenanceController.java
@@ -177,7 +177,8 @@ public class MaintenanceController {
             msg.setLocation(500, 500);
             return;
         }
-
+        mpanel.initCollectCash();
+        mpanel.initTotalCash();
         mpanel.setActive(MaintenancePanel.DIALOG, true);
 
     }
